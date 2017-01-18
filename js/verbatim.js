b45f014557a88b6341633cca658966c654a5abb1
James Yen <jameshyen@gmail.com>
2017-01-17 17:13:04 -0800

    preserve newlines

--- js/verbatim.js
+++ js/verbatim.js
@@ -1,46 +1,41 @@
 'use strict';
 
-/* function write() {
-    let lyrics = $("#lyrics").val();
-    console.log(lyrics);
-} */
-
-// $(window).on("load", write)
-
-// let re = /[^a-zA-Z0-9'’]*/;
-// let re = /[\s\n]*/;
-
-// let wordArray = lyrics.replace(/[\n\s]/, "~");
-// wordArray = lyrics.replace(/[^a-zA-Z0-9'’~]/, "");
-// wordArray = wordArray.split("~");
-
 $('#lyrics').on('keypress', function () {
   var lyrics = $('#lyrics').val();
 
-  var spaces = /[\s\n]/;
+  var space = /[\s]/;
+  var newline = /[\n]/;
   var words = /^[a-zA-Z0-9'‘’!]+$/;
   var endPunct = /[a-zA-Z0-9'‘’!]+[^a-zA-Z0-9'‘’!]$/;
   var startPunct = /^[^a-zA-Z0-9'‘’!][a-zA-Z0-9'‘’!]+/;
   var punct = /[^a-zA-Z0-9'‘’!]/;
 
-  var wordArray = lyrics.split(spaces);
+  var lineArray = lyrics.split(newline);
+  lyrics = '';
+  var wordArray = [];
   var newWordArray = [];
 
-  wordArray.forEach(function (word) {
-    if (words.test(word)) {
-      newWordArray.push(word);
-      // console.log(word);
-    } else if (startPunct.test(word) || endPunct.test(word)) {
-      var stripWord = word.replace(punct, '');
-      newWordArray.push(stripWord);
-      // console.log(stripWord);
-    } else {
-        // Pass.
-      }
+  lineArray.forEach(function (line) {
+    newWordArray = [];
+    wordArray = line.split(space);
+    wordArray.forEach(function (word) {
+      if (words.test(word)) {
+        newWordArray.push(word);
+        // console.log(word);
+      } else if (startPunct.test(word) || endPunct.test(word)) {
+        var stripWord = word.replace(punct, '');
+        newWordArray.push(stripWord);
+        // console.log(stripWord);
+      } else {
+          // Pass.
+        }
+    });
+    newWordArray = newWordArray.join(' ');
+    newWordArray += '\n';
+    // console.log(newWordArray);
+    lyrics += newWordArray;
   });
 
-  var markup = newWordArray.map(function (word) {
-    return '{' + word + '}';
-  });
-  $('#markup').html(markup);
+  // const markup = newWordArray.map(word => `{${word}}`);
+  $('#markup').html(lyrics);
 });
--- src/verbatim.js
+++ src/verbatim.js
@@ -1,42 +1,39 @@
-/* function write() {
-    let lyrics = $("#lyrics").val();
-    console.log(lyrics);
-} */
-
-// $(window).on("load", write)
-
-// let re = /[^a-zA-Z0-9'’]*/;
-// let re = /[\s\n]*/;
-
-// let wordArray = lyrics.replace(/[\n\s]/, "~");
-// wordArray = lyrics.replace(/[^a-zA-Z0-9'’~]/, "");
-// wordArray = wordArray.split("~");
-
 $('#lyrics').on('keypress', () => {
-  const lyrics = $('#lyrics').val();
+  let lyrics = $('#lyrics').val();
 
-  const spaces = /[\s\n]/;
+  const space = /[\s]/;
+  const newline = /[\n]/;
   const words = /^[a-zA-Z0-9'‘’!]+$/;
   const endPunct = /[a-zA-Z0-9'‘’!]+[^a-zA-Z0-9'‘’!]$/;
   const startPunct = /^[^a-zA-Z0-9'‘’!][a-zA-Z0-9'‘’!]+/;
   const punct = /[^a-zA-Z0-9'‘’!]/;
 
-  const wordArray = lyrics.split(spaces);
-  const newWordArray = [];
+  const lineArray = lyrics.split(newline);
+  lyrics = '';
+  let wordArray = [];
+  let newWordArray = [];
 
-  wordArray.forEach((word) => {
-    if (words.test(word)) {
-      newWordArray.push(word);
-      // console.log(word);
-    } else if (startPunct.test(word) || endPunct.test(word)) {
-      const stripWord = word.replace(punct, '');
-      newWordArray.push(stripWord);
-      // console.log(stripWord);
-    } else {
-      // Pass.
-    }
+  lineArray.forEach((line) => {
+    newWordArray = [];
+    wordArray = line.split(space);
+    wordArray.forEach((word) => {
+      if (words.test(word)) {
+        newWordArray.push(word);
+        // console.log(word);
+      } else if (startPunct.test(word) || endPunct.test(word)) {
+        const stripWord = word.replace(punct, '');
+        newWordArray.push(stripWord);
+        // console.log(stripWord);
+      } else {
+        // Pass.
+      }
+    });
+    newWordArray = newWordArray.join(' ');
+    newWordArray += '\n';
+    // console.log(newWordArray);
+    lyrics += newWordArray;
   });
 
-  const markup = newWordArray.map(word => `{${word}}`);
-  $('#markup').html(markup);
+  // const markup = newWordArray.map(word => `{${word}}`);
+  $('#markup').html(lyrics);
 });
