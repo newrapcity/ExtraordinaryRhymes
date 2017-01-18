0176bbf3768efa4cbbf7a040502ee88a31263a7e
James Yen <jameshyen@gmail.com>
2017-01-17 16:45:26 -0800

    Add ESLint

--- js/verbatim.js
+++ js/verbatim.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict';
 
-/*function write() {
-	let lyrics = $("#lyrics").val();
-	console.log(lyrics);
-}*/
+/* function write() {
+    let lyrics = $("#lyrics").val();
+    console.log(lyrics);
+} */
 
-//$(window).on("load", write)
+// $(window).on("load", write)
 
-//let re = /[^a-zA-Z0-9'’]*/;
-//let re = /[\s\n]*/;
+// let re = /[^a-zA-Z0-9'’]*/;
+// let re = /[\s\n]*/;
 
-//let wordArray = lyrics.replace(/[\n\s]/, "~");
-//wordArray = lyrics.replace(/[^a-zA-Z0-9'’~]/, "");
-//wordArray = wordArray.split("~");
+// let wordArray = lyrics.replace(/[\n\s]/, "~");
+// wordArray = lyrics.replace(/[^a-zA-Z0-9'’~]/, "");
+// wordArray = wordArray.split("~");
 
-$("#lyrics").on("keypress", function () {
-  var lyrics = $("#lyrics").val();
+$('#lyrics').on('keypress', function () {
+  var lyrics = $('#lyrics').val();
 
   var spaces = /[\s\n]/;
   var words = /^[a-zA-Z0-9'‘’!]+$/;
@@ -29,18 +29,18 @@ $("#lyrics").on("keypress", function () {
   wordArray.forEach(function (word) {
     if (words.test(word)) {
       newWordArray.push(word);
-      //console.log(word);
+      // console.log(word);
     } else if (startPunct.test(word) || endPunct.test(word)) {
-      word = word.replace(punct, "");
-      newWordArray.push(word);
-      console.log(word);
+      var stripWord = word.replace(punct, '');
+      newWordArray.push(stripWord);
+      // console.log(stripWord);
     } else {
-      //Pass.
-    }
+        // Pass.
+      }
   });
 
   var markup = newWordArray.map(function (word) {
-    return "{" + word + "}";
+    return '{' + word + '}';
   });
-  $("#markup").html(markup);
+  $('#markup').html(markup);
 });
--- src/verbatim.js
+++ src/verbatim.js
@@ -1,42 +1,42 @@
-/*function write() {
-	let lyrics = $("#lyrics").val();
-	console.log(lyrics);
-}*/
+/* function write() {
+    let lyrics = $("#lyrics").val();
+    console.log(lyrics);
+} */
 
-//$(window).on("load", write)
+// $(window).on("load", write)
 
-//let re = /[^a-zA-Z0-9'’]*/;
-//let re = /[\s\n]*/;
+// let re = /[^a-zA-Z0-9'’]*/;
+// let re = /[\s\n]*/;
 
-//let wordArray = lyrics.replace(/[\n\s]/, "~");
-//wordArray = lyrics.replace(/[^a-zA-Z0-9'’~]/, "");
-//wordArray = wordArray.split("~");
+// let wordArray = lyrics.replace(/[\n\s]/, "~");
+// wordArray = lyrics.replace(/[^a-zA-Z0-9'’~]/, "");
+// wordArray = wordArray.split("~");
 
-$("#lyrics").on("keypress", function() {
-  let lyrics = $("#lyrics").val();
+$('#lyrics').on('keypress', () => {
+  const lyrics = $('#lyrics').val();
 
-  let spaces = /[\s\n]/;
-  let words = /^[a-zA-Z0-9'‘’!]+$/;
-  let endPunct = /[a-zA-Z0-9'‘’!]+[^a-zA-Z0-9'‘’!]$/;
-  let startPunct = /^[^a-zA-Z0-9'‘’!][a-zA-Z0-9'‘’!]+/;
-  let punct = /[^a-zA-Z0-9'‘’!]/;
+  const spaces = /[\s\n]/;
+  const words = /^[a-zA-Z0-9'‘’!]+$/;
+  const endPunct = /[a-zA-Z0-9'‘’!]+[^a-zA-Z0-9'‘’!]$/;
+  const startPunct = /^[^a-zA-Z0-9'‘’!][a-zA-Z0-9'‘’!]+/;
+  const punct = /[^a-zA-Z0-9'‘’!]/;
 
-  let wordArray = lyrics.split(spaces);
-  let newWordArray = [];
+  const wordArray = lyrics.split(spaces);
+  const newWordArray = [];
 
-  wordArray.forEach(word => {
+  wordArray.forEach((word) => {
     if (words.test(word)) {
       newWordArray.push(word);
-      //console.log(word);
+      // console.log(word);
     } else if (startPunct.test(word) || endPunct.test(word)) {
-      word = word.replace(punct, "");
-      newWordArray.push(word);
-      console.log(word);
+      const stripWord = word.replace(punct, '');
+      newWordArray.push(stripWord);
+      // console.log(stripWord);
     } else {
-      //Pass.
+      // Pass.
     }
-  })
+  });
 
-  let markup = newWordArray.map(word => "{" + word + "}");
-  $("#markup").html(markup);
+  const markup = newWordArray.map(word => `{${word}}`);
+  $('#markup').html(markup);
 });
