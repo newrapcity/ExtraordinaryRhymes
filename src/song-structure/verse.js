37d2f64b5d8b9ddb09e85f8c384100f622817080
James Yen <jameshyen@gmail.com>
2017-01-30 17:36:42 -0800

    need typing

--- src/song-structure/verse.js
+++ src/song-structure/verse.js
@@ -4,6 +4,6 @@ class Verse {
       throw new VerseException('Purported size of verse does not match amount of bars given.');
     } else if ([16, 32, 64].indexOf(size) === -1) {
       throw new VerseException('Verse size is not of standard length.');
-    } else if (bars
+    } else if (bars.forEach(bar => {if (!(bar instanceof Bar)) {return false;}}))
   }
 }
