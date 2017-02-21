92904e8eacffdb3230ac4e12e5022203318785fe
James Yen <jameshyen@gmail.com>
2017-02-20 18:18:17 -0800

    didn't get everything

--- js/markup.js
+++ js/markup.js
@@ -20,19 +20,38 @@ function markup(idNoHashtag, partType) {
 
   switch (partType) {
     case 'bridge':
+      lyrics = BridgeProcess(song);
       break;
     case 'verse':
-      VerseProcess(song);
+      lyrics = VerseProcess(song);
       break;
     case 'intro':
+      lyrics = IntroProcess(song);
       break;
     case 'chorus':
+      // lyrics = ChorusProcess(song);
       break;
     case 'outro':
+      lyrics = OutroProcess(song);
       break;
     default:
+      // Leave as is.
       break;
   }
 
-  $('#markup').html(lyrics);
+  var decompId = /([a-zA-Z]*)([0-9]*)(-textarea)/;
+  var decompedId = decompId.exec(idNoHashtag);
+  var newId = decompedId[1] + decompedId[2];
+  if (decompedId[2] === '1') {
+    decompedId[2] = '';
+  }
+
+  var name = decompedId[1].charAt(0).toUpperCase() + decompedId[1].slice(1) + decompedId[2];
+  var header = '<h4 class=\"' + newId + '\">' + name + '</h4>';
+  var body = '<p style=\"white-space:pre-line;\" class=\"' + newId + '\">' + lyrics + '</p>';
+
+  $('#markup').children('.' + newId).remove();
+
+  $('#markup').append(header);
+  $('#markup').append(body);
 }
